4ba7269202ea2aef1aaacfad47d87290025e555b
Jean-Kelly Seguin <jean-kelly.seguin@outlook.fr>
2023-07-24 13:49:44 +0200

    Premier commit

---
+++ swyt-ssr/src/app/example-server-component.js
@@ -0,0 +1,11 @@
+export default function ExampleServerComponent() {
+  
+ 
+  return (
+    <>
+      <h1>Sign In</h1>
+      username: <input type="text" />
+      pasword: <input type="password" />
+    </>
+  )
+}
